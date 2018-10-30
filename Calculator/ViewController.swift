9aaa6bd9d5814283ed541afa7fb19a30b1628104
Arvin Zojaji <arvin.zojaji@gmail.com>
2018-10-30 15:04:18 -0400

    Fixed files that were ignored as submodules

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Arvin Zojaji on 2018-10-29.
+//  Copyright © 2018 Arvin Zojaji. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
